9c1249351211e456bff203ef3775fee15a69d457
BrofessorTec <tcampbell59@yahoo.com>
2024-02-12 17:20:06 -0500

    end of checkpoint 3

--- TCInteractiveGraphicsS24/Texture.cpp
+++ TCInteractiveGraphicsS24/Texture.cpp
@@ -1,4 +1,5 @@
 #include "Texture.h"
+#include "glad/glad.h"
 
 /*
 void Texture::SetTextureData(unsigned int count, unsigned char* data)
@@ -7,3 +8,52 @@ void Texture::SetTextureData(unsigned int count, unsigned char* data)
 	memcpy(data, textureData, count);
 }
 */
+
+Texture::Texture()
+{
+	textureData = nullptr;
+	glGenTextures(1, &textureId);
+}
+
+
+void Texture::SetTextureData(unsigned int count, unsigned char* data)
+{
+	CleanUp();
+	textureData = new unsigned char[count];
+	memcpy(data, textureData, count);
+	isLoadedFromFile = false;
+}
+
+bool Texture::IsLoadedFromFile()
+{
+	return isLoadedFromFile;
+}
+
+unsigned int Texture::TextureId()
+{
+	return textureId;
+}
+
+Texture::~Texture()
+{
+	CleanUp();
+}
+
+unsigned char* Texture::GetTextureData()
+{
+	return textureData;
+}
+
+void Texture::CleanUp()
+{
+	if (textureData == nullptr)
+	{
+		return;
+	}
+	if (!isLoadedFromFile)
+	{
+		delete[] textureData;
+	}
+
+	textureData = nullptr;
+}
--- TCInteractiveGraphicsS24/Texture.h
+++ TCInteractiveGraphicsS24/Texture.h
@@ -1,6 +1,24 @@
 #pragma once
 #include "BaseObject.h"
 
+class Texture :
+    public BaseObject
+{
+private:
+    unsigned char* textureData;
+    bool isLoadedFromFile = false;
+    unsigned int textureId;
+public:
+    Texture();
+    ~Texture();  //calls cleanup
+    unsigned char* GetTextureData();
+    void SetTextureData(unsigned int count, unsigned char* data);
+    bool IsLoadedFromFile();
+    unsigned int TextureId();
+private:
+    void CleanUp();
+};
+
 
 /*
 class Texture :
