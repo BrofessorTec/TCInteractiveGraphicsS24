67c884e145078d9b991702ae63c7798e22188004
BrofessorTec <125778428+BrofessorTec@users.noreply.github.com>
2024-02-28 16:54:37 -0500

    Updated

--- TCInteractiveGraphicsS24/Camera.cpp
+++ TCInteractiveGraphicsS24/Camera.cpp
@@ -12,17 +12,17 @@ void Camera::SetMoveSpeed(int speed)
 
 void Camera::SetPosition(glm::vec3 position)
 {
-	glm::vec4 posUpdate = { position[0], position[1], position[2], 1.0f };  // is last spot a 0 or 1? cant remember
-	refFrame[3] = posUpdate;
+	//glm::vec4 posUpdate = { position[0], position[1], position[2], 1.0f };  // is last spot a 0 or 1? cant remember
+	refFrame[3] = glm::vec4(position, 1.0f);
 }
 
 glm::mat4 Camera::LookForward()
 {
 	// how to do this?
 	//returns a view matrix that looks forward. Use the look frame
+	lookFrame[2] = glm::vec4(1.0f);  // would this accomplish it? or needing to normalize?
 
-
-	return glm::mat4();
+	return lookFrame;
 }
 
 glm::mat4 Camera::LookAtTarget()
--- TCInteractiveGraphicsS24/RotateAnimation.h
+++ TCInteractiveGraphicsS24/RotateAnimation.h
@@ -11,5 +11,5 @@ public:
 	RotateAnimation(glm::vec3 axis = glm::vec3(0.0f, 1.0f, 0.0f), float speed
 		= 90.0f)
 		: IAnimation(), axis(axis), speed(speed) {}
-	void Update(double elapsedSeconds);
+	void Update(double elapsedSeconds) override;
 };
