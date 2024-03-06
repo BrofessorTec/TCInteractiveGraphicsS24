764dea4ae08e07307e6e6a317b46fb0450449c93
BrofessorTec <125778428+BrofessorTec@users.noreply.github.com>
2024-03-06 13:29:31 -0500

    updated uniforms

--- TCInteractiveGraphicsS24/Renderer.h
+++ TCInteractiveGraphicsS24/Renderer.h
@@ -60,7 +60,7 @@ public:
         glBindVertexArray(0);
     }
     
-    void RenderScene(Camera const &camera)
+    void RenderScene(Camera& camera)
     {
         if (shader->IsCreated()) {
             glUseProgram(shader->GetShaderProgram());
@@ -68,6 +68,31 @@ public:
             //glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
             shader->SendMat4Uniform("view", view);
             shader->SendMat4Uniform("projection", projection);
+            shader->SendVec3Uniform("cameraPos", glm::value_ptr(camera)->GetPosition());  // need to fix this
+            shader->SendFloatUniform("globalLightAttenuation", scene->GetGlobalLight().attenuationCoef);
+            shader->SendFloatUniform("localLightAttenuation", scene->GetLocalLight().attenuationCoef);
+            shader->SendFloatUniform("globalLightIntensity", scene->GetGlobalLight().intensity);
+            shader->SendFloatUniform("localLightIntensity", scene->GetLocalLight().intensity);
+            shader->SendVec3Uniform("globalLightColor", scene->GetGlobalLight().color);
+            shader->SendVec3Uniform("localLightColor", scene->GetLocalLight().color);
+            shader->SendVec3Uniform("globalLightPos", scene->GetGlobalLight().position);
+            shader->SendVec3Uniform("localLightPos", scene->GetLocalLight().position);
+
+
+            /* values needed from the notes
+            glUniform3fv(globalLightPosLoc, 1, glm::value_ptr(globalLight.position));
+            glUniform3fv(globalLightColorLoc, 1, glm::value_ptr(globalLight.color));
+            glUniform1f(globalLightIntensityLoc, globalLight.intensity);
+            glUniform3fv(localLightPosLoc, 1, glm::value_ptr(localLight.position));
+            glUniform3fv(localLightColorLoc, 1, glm::value_ptr(localLight.color));
+            glUniform1f(localLightIntensityLoc, localLight.intensity);
+            glUniform1f(localLightAttentuationLoc, localLight.attenuationCoef);
+            glUniform3fv(viewPositionLoc, 1, glm::value_ptr(cameraPosition));
+            
+            
+            */
+
+
             // Render the objects in the scene
             for (auto& object : scene->GetObjects()) {
                 RenderObject(*object);
